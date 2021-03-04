cc715e2550b4b26f970f5fbf160593ce23b7638b
AlBan52 <balakin.nn@gmail.com>
2021-03-04 20:50:59 +0300

    The function name 'get_hubble_images_id()' changed to 'get_hubble_images_ids()''

--- hubble_fetch.py
+++ hubble_fetch.py
@@ -7,16 +7,16 @@ from dotenv import load_dotenv
 urllib3.disable_warnings()
 
 
-def get_hubble_images_id():
+def get_hubble_images_ids():
 
     payload = {'page': 'all', 'collection_name': 'spacecraft'}
     response = requests.get(
         'http://hubblesite.org/api/v3/images', params=payload)
     response.raise_for_status()
     image_files_descriptions = response.json()
-    images_id = [image['id'] for image in image_files_descriptions]
+    images_ids = [image['id'] for image in image_files_descriptions]
 
-    return images_id
+    return images_ids
 
 
 def download_hubble_images_link(image_id):
@@ -45,9 +45,9 @@ if __name__ == '__main__':
     file_path = os.getenv('IMAGES')
     os.makedirs(file_path, exist_ok=True)
 
-    images_id = get_hubble_images_id()
+    images_ids = get_hubble_images_ids()
     print('Downloading hubbles images start...')
-    for image_id in images_id:
+    for image_id in images_ids:
         image_link = download_hubble_images_link(image_id)
         name_link, image_exten = os.path.splitext(image_link)
         download_hubble_images(file_path, image_link, image_id, image_exten)
